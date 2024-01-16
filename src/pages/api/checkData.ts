7b707dc9d49cd79288e5d17f520e917302d0a8e2
mohansharma916 <mohansharma916@gmail.com>
2024-01-16 22:07:28 +0530

    link changed

--- src/pages/api/checkData.ts
+++
@@ -1,6 +0,0 @@
-export default (req, res) => {
-  const isLocal = process.env.NODE_ENV === "development";
-  const currentUrl = `${req.headers.host}`;
-
-  res.status(200).json({ isLocal, currentUrl });
-};
